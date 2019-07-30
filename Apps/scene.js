62c7c737475f417a5b1d49bcf3e01e9a9a0b9b7c
jswens912 <jswens912@gmail.com>
2019-07-30 10:57:37 -0500

    added local storage functions

--- Apps/scene.js
+++ Apps/scene.js
@@ -74,12 +74,28 @@ function renderScene(parent, story, sceneName) {
   parent.innerHTML = '';
   parent.appendChild(sceneElement);
   currentStory.innerHTML='';
-  currentStory.push(parent);
-  currentStory.push(story);
   currentStory.push(sceneName);
-}
-function saveStory(){
-  localStorage.setItem("currentStory", JSON.stringify(currentStory))
 }
 
 renderScene(document.getElementById('scene-root'), testStory, 'start');
+clearStory();
+saveStory();
+
+function saveStory(){
+  localStorage.setItem("currentStory", currentStory);
+  console.log(localStorage);
+}
+function clearStory(){
+  localStorage.innerHTML = '';
+};
+
+function setStory(){
+  var storage = localStorage['currentStory'];
+    if(storage){
+      currentStory = storage;
+      renderScene(currentStory);
+    }
+    else{
+      return;
+    }
+};
